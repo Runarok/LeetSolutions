02579cde78a939357a39d11efa7ff455838ae398
Runarok <RunarokHrafn@gmail.com>
2025-04-04 18:10:36 +0530

    Time: 1 ms (42.86%) | Memory: 20 MB (50.00%) - LeetSync

---
+++ Solutions/1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.swift
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+
+class Solution {
+    func lcaDeepestLeaves(_ root: TreeNode?) -> TreeNode? {
+        // Helper function for DFS
+        func dfs(_ node: TreeNode?) -> (Int, TreeNode?) {
+            guard let node = node else {
+                return (0, nil) // Base case: null node
+            }
+            
+            let (leftDepth, leftLCA) = dfs(node.left)
+            let (rightDepth, rightLCA) = dfs(node.right)
+            
+            // If both left and right subtrees have the same depth, current node is the LCA
+            if leftDepth == rightDepth {
+                return (leftDepth + 1, node)
+            }
+            
+            // Propagate the deeper subtree
+            if leftDepth > rightDepth {
+                return (leftDepth + 1, leftLCA)
+            } else {
+                return (rightDepth + 1, rightLCA)
+            }
+        }
+        
+        return dfs(root).1
+    }
+}
