518cfacc0b14bdabfc03bd6c3c31a97069f74d78
Runarok <RunarokHrafn@gmail.com>
2026-01-05 10:56:49 +0530

    Time: 366 ms (37.44%) | Memory: 0B (100.00%) - LeetSync

---
+++ Solutions/601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+
+WITH cte AS (
+    SELECT *,
+           LEAD(people, 1) OVER (ORDER BY id) AS next1,
+           LEAD(people, 2) OVER (ORDER BY id) AS next2,
+           LAG(people, 1) OVER (ORDER BY id) AS prev1,
+           LAG(people, 2) OVER (ORDER BY id) AS prev2
+    FROM Stadium
+)
+SELECT id, visit_date, people
+FROM cte
+WHERE people >= 100
+  AND (
+       (people >= 100 AND next1 >= 100 AND next2 >= 100)   -- start of a sequence
+    OR (prev1 >= 100 AND people >= 100 AND next1 >= 100)  -- middle of a sequence
+    OR (prev2 >= 100 AND prev1 >= 100 AND people >= 100)  -- end of a sequence
+  )
+ORDER BY visit_date;
