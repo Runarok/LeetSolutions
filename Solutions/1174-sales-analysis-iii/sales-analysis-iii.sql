69edcc398fdaa2804f4d6ed90fdf36264edd155f
Runarok <RunarokHrafn@gmail.com>
2026-01-23 07:47:06 +0530

    Time: 1079 ms (65.34%) | Memory: 0B (100.00%) - LeetSync

---
+++ Solutions/1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+SELECT p.product_id, p.product_name
+FROM Product p
+JOIN Sales s ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING 
+    -- All sales are in Q1 2019
+    MIN(s.sale_date) >= '2019-01-01'
+    AND MAX(s.sale_date) <= '2019-03-31';
